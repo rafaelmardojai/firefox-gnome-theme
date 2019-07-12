926e78bc50c14b57e850e3f87992bbbdd0a305b1
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2019-07-12 11:37:44 -0500

    Add user.js proof of concept

---
+++ configuration/user.js
@@ -0,0 +1,9 @@
+/* user.js
+ * https://github.com/rafaelmardojai/firefox-gnome-theme/
+ */
+
+// Enable customChrome.css
+user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
+
+// Enable CSD
+user_pref("browser.tabs.drawInTitlebar", true);
