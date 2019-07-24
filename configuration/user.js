abce2c32b432c9b0b518487c4c1185e4f3a64bce
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2019-07-24 14:21:09 -0500

    User.js: Set UI density to normal, fix #42

--- configuration/user.js
+++ configuration/user.js
@@ -8,3 +8,6 @@ user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 // Enable CSD
 user_pref("browser.tabs.drawInTitlebar", true);
 
+// Set UI density to normal
+user_pref("browser.uidensity", 0);
+
