05ae3d960c15d75dcb49a772fce6d4917adb5306
Rafael Mardojai CM <mardojai.cardenas@gmail.com>
2021-05-15 08:45:53 -0500

    Don't force CSD on user.js fix #194

--- configuration/user.js
+++ configuration/user.js
@@ -5,9 +5,6 @@
 // Enable customChrome.css
 user_pref("toolkit.legacyUserProfileCustomizations.stylesheets", true);
 
-// Enable CSD
-user_pref("browser.tabs.drawInTitlebar", true);
-
 // Set UI density to normal
 user_pref("browser.uidensity", 0);
 
