73bb9eb1db27f3dadfaf12f89ef3fa5778e4aa83
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-05-18 20:42:57 +0200

    Update install-by-curl.sh

--- scripts/install-by-curl.sh
+++ scripts/install-by-curl.sh
@@ -1,22 +1,16 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
 VERSION=$(curl --silent "https://api.github.com/repos/rafaelmardojai/firefox-gnome-theme/releases/latest" | grep tag_name | cut -d'"' -f4)
 FILENAME=firefox-gnome-theme-$VERSION.tar.gz
-FOLDERPATH=$PWD/firefox-gnome-theme
 
-if [ -d "$FOLDERPATH" ]; then rm -Rf $FOLDERPATH; fi
-
-mkdir $FOLDERPATH
-
-cd $FOLDERPATH
+(
 
 curl -LJo $FILENAME https://github.com/rafaelmardojai/firefox-gnome-theme/tarball/$VERSION
 
 tar -xzf $FILENAME --strip-components=1
-rm $FILENAME
 
 chmod +x scripts/auto-install.sh
 
 ./scripts/auto-install.sh
 
-if [ -d "$FOLDERPATH" ]; then rm -Rf $FOLDERPATH; fi
+)
