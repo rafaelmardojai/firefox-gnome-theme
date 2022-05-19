46fa89c92cd43f4cbff682420855d478e877e3ac
Silejonu <48565914+Silejonu@users.noreply.github.com>
2022-05-19 21:15:01 +0200

    Fix install-by-curl.sh (#380)
    - Clone repo in temp location

--- scripts/install-by-curl.sh
+++ scripts/install-by-curl.sh
@@ -1,22 +1,20 @@
-#! /usr/bin/env bash
+#!/usr/bin/env bash
 
-VERSION=$(curl -s "https://github.com/rafaelmardojai/firefox-gnome-theme/releases/latest/download" 2>&1 | sed "s/^.*download\/\([^\"]*\).*/\1/")
+VERSION=$(curl --silent "https://api.github.com/repos/rafaelmardojai/firefox-gnome-theme/releases/latest" | grep tag_name | cut -d'"' -f4)
 FILENAME=firefox-gnome-theme-$VERSION.tar.gz
-FOLDERPATH=$PWD/firefox-gnome-theme
 
-if [ -d "$FOLDERPATH" ]; then rm -Rf $FOLDERPATH; fi
+(
 
-mkdir $FOLDERPATH
-
-cd $FOLDERPATH
+cd $(mktemp -d) || exit 1
+mkdir firefox-gnome-theme
+cd firefox-gnome-theme
 
 curl -LJo $FILENAME https://github.com/rafaelmardojai/firefox-gnome-theme/tarball/$VERSION
 
 tar -xzf $FILENAME --strip-components=1
-rm $FILENAME
 
 chmod +x scripts/auto-install.sh
 
 ./scripts/auto-install.sh
 
-if [ -d "$FOLDERPATH" ]; then rm -Rf $FOLDERPATH; fi
+)
