6839d9e9d7fa414b34f918da3f6c4b05769a3ab0
boerdereinar <einardegruyl@gmail.com>
2023-11-09 23:03:22 +0100

    window: Enable rounded bottom window corners

--- configuration/user.js
+++ configuration/user.js
@@ -14,3 +14,6 @@ user_pref("svg.context-properties.content.enabled", true);
 // Disable private window dark theme
 user_pref("browser.theme.dark-private-windows", false);
 
+// Enable rounded bottom window corners
+user_pref("widget.gtk.rounded-bottom-corners.enabled", true);
+
